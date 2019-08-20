9fb6488a4eb6e8de1fd47372ab07875e73a0b2c8
Molly <15380135366@163.com>
2019-08-20 10:19:08 +0800

    Add files via upload

---
+++ cds2pro.py
@@ -0,0 +1,18 @@
+from Bio import SeqIO
+import argparse
+
+def cds2pro(input, output):
+    with open(output, 'w') as f1:
+        for seq_record in SeqIO.parse(input, "fasta"):
+            f1.write('>'+seq_record.id+'\n')
+            f1.write(str(seq_record.seq.translate(table=11))+'\n')
+
+def main():
+    parser = argparse.ArgumentParser(description='This Script is to convert nucleotide to amino acid')
+    parser.add_argument('-i', '--input', type=str, metavar='', required=True, help='cds.fasta')
+    parser.add_argument('-o', '--output', type=str, metavar='', required=True, help='protein.fasta')
+    args = parser.parse_args()
+    cds2pro(args.input, args.output)
+
+if __name__ == "__main__":
+    main()
