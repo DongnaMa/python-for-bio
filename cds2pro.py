eeca29660d03cc4d07f65237b7cfe631929b2f45
Molly <15380135366@163.com>
2019-08-20 10:30:40 +0800

    Update cds2pro.py

--- cds2pro.py
+++ cds2pro.py
@@ -8,7 +8,7 @@ def cds2pro(input, output):
             f1.write(str(seq_record.seq.translate(table=11))+'\n')
 
 def main():
-    parser = argparse.ArgumentParser(description='This Script is to convert nucleotide to amino acid')
+    parser = argparse.ArgumentParser(description='This script is to convert nucleotide to amino acid')
     parser.add_argument('-i', '--input', type=str, metavar='', required=True, help='cds.fasta')
     parser.add_argument('-o', '--output', type=str, metavar='', required=True, help='protein.fasta')
     args = parser.parse_args()
