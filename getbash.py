9ce771af9190ee1d2629624a12d4de29047ec537
Molly <dongnama@foxmail.com>
2019-11-17 21:24:18 +0800

    Add files via upload

---
+++ getbash.py
@@ -0,0 +1,7 @@
+for i in range(261):
+    i_str = str(i+1)
+    file_name = 'run'+i_str + '.sh'
+    f = open('1/'+file_name,'w')
+    f.write('#!/bin/bash'+'\n'+'#$ -cwd'+'\n'+'#$ -S /bin/bash'+'\n'+'#$ -j y'+'\n'+'#$ -pe mpi 1'+'\n'+'\n')
+    f.write('raxmlHPC-PTHREADS  -T 1 -m GTRGAMMA --HKY85  -f a -N 100 -p 8355 -x 2887 -s ../input/phylip/'+str(i+1)+'.phy -n '+str(i+1))
+    f.close()
