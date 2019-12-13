56ef953ca9301f8ad4289be85e5b37b67ffac4df
Molly <dongnama@foxmail.com>
2019-12-13 17:59:31 +0800

    Add files via upload

---
+++ getfasta2.py
@@ -0,0 +1,34 @@
+import argparse
+
+def getfasta(genelist, database, output):
+    dic = {}
+    query = []
+    with open(database, 'r') as f1:
+        for line1 in f1:
+            line1 = line1.strip()
+            if line1.startswith('>'):
+               name = line1[1:]
+               name1 = name.split('|')[1]
+               dic[name1] = ''
+            else:
+               dic[name1] += line1
+    with open(genelist, 'r') as f2:
+        for geneID in f2:
+            query.append(geneID.strip())
+    with open(output, 'w') as f3:
+        for line2 in query:
+            if line2 in dic.keys():
+                f3.write(">" + line2 + "\n" + dic[line2].strip()+"\n")
+            else:
+                pass
+
+def main():
+    parser = argparse.ArgumentParser(description='From gene list get fasta sequence')
+    parser.add_argument('-l', '--genelist', type=str, metavar='', required=True, help='Note the the name of gene should be same with the database')
+    parser.add_argument('-d', '--database', type=str, metavar='', required=True, help='fasta file')
+    parser.add_argument('-o', '--output', type=str, metavar='', required=True, help='output file')
+    args = parser.parse_args()
+    getfasta(args.genelist, args.database, args.output)
+
+if __name__ == "__main__":
+    main()
