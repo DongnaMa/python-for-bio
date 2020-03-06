2e7fec5e6d2ffb10d0f3dab365c369f42598ee97
Molly <dongnama@foxmail.com>
2020-03-06 16:37:55 +0800

    Update rename_fasta.py

--- rename_fasta.py
+++ rename_fasta.py
@@ -6,7 +6,7 @@ usage: python input.fasta out.fasta
 import sys
 
 i = 1
-with open(sys.argv[1], 'r') as f1 , open(sys.argv[2], 'r') as f2:
+with open(sys.argv[1], 'r') as f1 , open(sys.argv[2], 'w') as f2:
     for line in f1:
         line = line.strip()
         if line.startswith('>'):
