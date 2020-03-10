00f678a4ec5813681162d24c8e804f3e2c7668ed
Molly <dongnama@foxmail.com>
2020-03-10 21:41:57 +0800

    Update merge_fasta_from_files.py

--- merge_fasta_from_files.py
+++ merge_fasta_from_files.py
@@ -1,19 +1,16 @@
 import os
+import os.path
 
-#文件夹目录
-path = "C:/Users/牛奶变咖啡/Desktop/tree_result/fa"
-#得到文件夹下面的所有文件名称
-files = os.listdir(path)
-s = []
-for file in files:
-    if not os.path.isdir(file): #判断是否是文件夹，不是文件夹才打开
-        f = open(path+'/'+file)
-        iter_f = iter(f) #创建迭代器
-        a = ''
-        for line in iter_f: #遍历文件，一行行遍历，读取文本
-            a = a + line
-        s.append(a) #每个文件的文本保存到list中
+with open('out.tab', 'w') as result_file:
+    # 文件夹目录
+    path = "C:/Users/牛奶变咖啡/Desktop/tree_result/fa"
+    # 得到文件夹下面的所有文件名称
+    files = os.listdir(path)
 
-with open(path+'/'+'all.txt', 'w') as result_file:
-    s = ''.join(s)
-    result_file.write(s)
+    for file in files:
+        if not os.path.isdir(file):  # 判断是否是文件夹，不是文件夹才打开
+            if os.path.splitext(file)[1] == '.out':
+                f = open(path + '/' + file)
+                for line in f:
+                    line = line.strip()
+                    result_file.write(line + "\n")
