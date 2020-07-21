5a4dbdd100791883414cb2da352e999118e1b183
Luffy <dongnama@foxmail.com>
2020-07-21 14:54:29 +0800

    Update filter_gff3.py

--- filter_gff3.py
+++ filter_gff3.py
@@ -5,12 +5,12 @@ This script can Filter gff3 files
 import sys
 import re
 
-USAGE = "\nusage: python %s filter_gff3.py ID.txt input.gff3 out.gff3\n" % sys.argv[0]
+USAGE = "\nusage: python %s ID.file inputfile outputfile\n" % sys.argv[0]
 
 if len(sys.argv) != 4:
     print(USAGE)
     sys.exit()
-
+    
 query = []
 
 with open(sys.argv[1], 'r') as fp1:
@@ -18,16 +18,16 @@ with open(sys.argv[1], 'r') as fp1:
         l1 = l1.strip()
         query.append(l1)
 
-with open(sys.argv[2], 'r') as fp2 , open(sys.argv[3], 'w') as fp3:
+with open(sys.argv[2], 'r') as fp2, open(sys.argv[3], 'w') as fp3:
+    fp3.write("###gff version 3\n")
     for l2 in fp2:
-        l2 = l2.strip()
-        # re.split 多个分隔符分割
-        name = re.split('[=;]',l2)[1]
+        if l2[0] == '#' or l2.strip() == '':
+            continue
+        name = re.split(r'=|;|-mRNA', l2)[1]
         if name in query:
             continue
         else:
-            fp3.write(l2 + '\n')
-
+            fp3.write(l2)
 
 
 
