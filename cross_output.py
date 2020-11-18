c544cae5fdabc7c85b979a29a232ed4b7056293a
Luffy <dongnama@foxmail.com>
2020-11-18 15:06:23 +0800

    Update cross_output.py

--- cross_output.py
+++ cross_output.py
@@ -2,9 +2,9 @@
 
 import sys
 from itertools import zip_longest
-file1 = open('sys.argv[1]', 'r')
-file2 = open('sys.argv[2]', 'r')
-out_result = open('sys.argv[3]', 'w')
+file1 = open(sys.argv[1], 'r')
+file2 = open(sys.argv[2], 'r')
+out_result = open(sys.argv[3], 'w')
 for line1,line2 in zip_longest(file1,file2):
     line1 = line1.strip()
     line2 = line2.strip()
